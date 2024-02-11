a07764a62962bfb2c2ff103c0b17f4d77bd327af
camiloruiz <camiloruizvidal@gmail.com>
2024-02-10 21:02:15 -0500

    added limit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(json({ limit: '100mb' }));
   const port = 3000;
-  console.log({ port });
   await app.listen(port);
 }
 bootstrap();
